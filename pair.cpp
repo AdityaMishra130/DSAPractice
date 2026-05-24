15e75cc8de80657b5c58fbcc82ea10009ef482ce
Adi <meethemis2006@gmail.com>
2026-05-24 14:26:11 +0530

    Done with vector and pair

---
+++ pair.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    pair<int, string>p;
+    // p = make_pair(1, "Dex");
+    // or  
+    // p = {2, "Debra"}; //other way to initialise pair
+    // cout << p.first << " " << p.second <<  endl;
+    // pair<int, string> p1 = p;
+    // p1.first = 3;
+    // cout << p1.first << " " << p1.second << endl;
+
+    // int a[] = {1, 2, 3};
+    // int b[] = {2, 3, 4};
+    // pair<int , int> p_array[3];
+    // p_array[0] = {1, 2};
+    // p_array[1] = {2, 3};
+    // p_array[2] = {3, 4};
+    // swap(p_array[0], p_array[2]);
+    // for(int i=0; i<3; i++)
+    // {
+    //     cout << p_array[i].first << " " << p_array[i].second << endl;
+    // }
+
+    cin >> p.first;
+    cout << p.first << endl;
+}
